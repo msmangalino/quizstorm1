ec6090995a958aaa2d4ffc06ed8c892010387b83
xyrhene mangalino <mangalinoxy@gmail.com>
2021-11-08 11:16:23 +0800

    summary

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented the Light Bulb?",
+    answer: "Thomas Alva Edison",
+    options: [
+      "Thomas Alva Edison",
+      "John Dewey",
+      "Bill Gates",
+      "Thomas Steart"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which planet in our solar system is known as the Red Planet?",
+    answer: "Mars",
+    options: [
+      "Mercury",
+      "Jupiter",
+      "Venus",
+      "Mars"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many rings are on the Olympic flag?",
+    answer: "5",
+    options: [
+      "4",
+      "None",
+      "5",
+      "7"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is a Tarsier?",
+    answer: "A primate",
+    options: [
+      "A lizard",
+      "A primate",
+      "A bird",
+      "A insect"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In darts, what's the most points you can score with a single throw?",
+    answer: "60",
+    options: [
+      "20",
+      "50",
+      "60",
+      "100"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
